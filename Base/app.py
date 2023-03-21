d94f1418a449ec91f3f7e97d36f1f571979f382a
khalilBedd <97263154+khbed@users.noreply.github.com>
2023-03-21 17:14:08 -0400

    Update app.py

--- Base/app.py
+++ Base/app.py
@@ -16,6 +16,17 @@ def index():
     max_date = interventions_df.iloc[0]['DATE_INCIDENT']
     min_date = interventions_df.iloc[-1]['DATE_INCIDENT']
 
+    # On va chercher la vaeur de modify_no_intervention
+    modify_no_intervention = request.args.get('modify_no_intervention')
+    # Si modify_no_intervention a une valeur non nulle on l'imprime sur la console
+    if modify_no_intervention != None:
+        print(modify_no_intervention)
+
+    # On va chercher la vaeur de remove_no_intervention
+    remove_no_intervention = request.args.get('remove_no_intervention')
+    # Si remove_no_intervention a une valeur non nulle on l'imprime sur la console
+    if remove_no_intervention != None:
+        print(remove_no_intervention)
 
     return render_template('Base_TP3.html', pdq_df=pdq_df, nombre_interventions=nombre_interventions.to_dict(), min_date=min_date, max_date=max_date)
 
